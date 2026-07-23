4603b581a7c05c6074a63d1e15c722b817329870
nikhil <n77794474@gmail.com>
2026-07-23 18:43:35 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+const SUPABASE_URL = "https://fclkjwqdcihjvvwhgvlm.supabase.co";
+const SUPABASE_KEY = "sb_publishable_NsDjDaDKZb_ix3HRvebNoQ_3Gewb75-";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
